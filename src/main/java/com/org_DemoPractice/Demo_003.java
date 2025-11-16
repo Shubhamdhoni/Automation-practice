0a1481b41ecb8313d88dcfb9c2ded2ae86094273
Shubhamdhoni <shubhamdhoni030@gmail.com>
2025-11-16 15:22:23 +0530

    New test file edited

---
+++ src/main/java/com/org_DemoPractice/Demo_003.java
@@ -0,0 +1,27 @@
+
+package com.org_DemoPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo_003 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+		
+		WebElement a = driver.findElement(By.xpath("//h1[text()='Practice Page']"));
+		System.out.println(a.getText());
+		
+		Thread.sleep(20);
+		driver.close();
+		
+	}
+
+}
